e2652dd4a0291d373fec358c78075523b42e36db
kraynel <kraynel@yahoo.fr>
2017-12-16 17:08:27 +0100

    Change action to async/await

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,22 +1,17 @@
 import * as types from './mutation-types'
 
 export const toggleAction = async ({ commit, state }) => {
-  state.upsideDownPromise
-    .enter()
-    .then(() => {
-      commit(types.UPDATE_STATUS, 'In upside down')
-      return state.upsideDownPromise.findDemogorgon()
-    })
-    .then(location => {
-      commit(types.UPDATE_STATUS, `Demogorgon in ${location}`)
-      return state.upsideDownPromise.killDemogorgon()
-    })
-    .then(result => {
-      if (result === 'SUCCESS') {
-        commit(types.UPDATE_STATUS, 'Demogorgon dead ️☠️')
-        return
-      }
+  await state.upsideDownPromise.enter()
+  commit(types.UPDATE_STATUS, 'In upside down')
 
-      commit(types.UPDATE_STATUS, 'You are dead ☠️')
-    })
+  const location = await state.upsideDownPromise.findDemogorgon()
+  commit(types.UPDATE_STATUS, `Demogorgon in ${location}`)
+
+  const result = await state.upsideDownPromise.killDemogorgon()
+  if (result === 'SUCCESS') {
+    commit(types.UPDATE_STATUS, 'Demogorgon dead ️☠️')
+    return
+  }
+
+  commit(types.UPDATE_STATUS, 'You are dead ☠️')
 }
